93b139790e4d3e17154b06df57d3179cfc8776ed
t4e1 <jtw5738@naver.com>
2025-01-01 07:00:58 +0900

    Firt touch

---
+++ DesignPattern/dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("안녕하세요");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Rlc2lnbnBhdHRlcm4vLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCLslYjrhZXtlZjshLjsmpRcIilcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ DesignPattern/webpack.config.js
@@ -0,0 +1,26 @@
+// webpack  
+// 여러 개의 소스 파일을 하나의 소스 파일로 묶어주는 기능
+
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.ts",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",  // 하나로 묶인 파일의 이름을 bundle.js로 지정
+        path: path.resolve(__dirname, "dist"),
+    },
+}
